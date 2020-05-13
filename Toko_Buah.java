55bfa53715ac5064e2d2ad348e8027ea1b6e264b
ardimanalu <65146887+ardimanalu15@users.noreply.github.com>
2020-05-13 12:17:48 +0700

    Add files via upload

---
+++ Toko_Buah.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toko_buah;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ardi manalu
+ */
+public class Toko_Buah {
+    
+  private static Connection koneksi;
+  public static Connection tokobuahDB()throws SQLException {
+      try {
+          String url = "jdbc:mysql://localhost:3306/tokobuah";
+          String user = "root";
+          String password = "";
+          
+          DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+          koneksi = DriverManager.getConnection(url, user, password);
+          
+      }catch(SQLException e) {
+          System.out.println(" koneksi ke database gagal " + e.getMessage());
+      }
+      return koneksi;
+  }
+}
